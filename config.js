328208b3d9421b541386ee1d6b745e730b085bc0
AmmarSep <ssammar114@gmail.com>
2026-02-22 12:49:33 +0530

    Update Supabase configuration for prayer timings

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MPT_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://mkbuqfumjunfehpdbdpz.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rYnVxZnVtanVuZmVocGRiZHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzY5MzAsImV4cCI6MjA4NzMxMjkzMH0.i2ToQ0pxW1Pn1zjg_sWk5tc0-SdnHVfyIzjqhkWZ9Po",
   supabaseTable: "prayer_timings",
-  supabaseRecordId: "global"
+  supabaseRecordId: "main"
 };
